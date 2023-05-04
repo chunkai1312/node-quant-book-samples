ab3b30fddb51e287c1d3c0a451e28c5aebdf477f
Chun-Kai Wang <chunkai1312@gmail.com>
2023-05-04 22:01:55 +0800

    feat: add chapter 7

---
+++ ch07/scraper/src/scraper/tpex-scraper.service.ts
@@ -0,0 +1,40 @@
+import * as numeral from 'numeral';
+import { DateTime } from 'luxon';
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class TpexScraperService {
+  constructor(private httpService: HttpService) {}
+
+  async fetchMarketTrades(date: string) {
+    const dt = DateTime.fromISO(date);
+    const formattedDate = `${dt.get('year') - 1911}/${dt.toFormat('MM/dd')}`;
+    const query = new URLSearchParams({ d: formattedDate, o: 'json' });
+    const url = `https://www.tpex.org.tw/web/stock/aftertrading/daily_trading_index/st41_result.php?${query}`;
+
+    const data = await firstValueFrom(this.httpService.get(url))
+      .then(response => (response.data.iTotalRecords > 0) && response.data);
+
+    return data?.aaData
+      ?.map(row => {
+        const [ date, ...values ] = row;
+        const [ year, month, day ] = date.split('/');
+        const formattedDate = `${+year + 1911}-${month}-${day}`;
+
+        const [ tradeVolume, tradeValue, transaction, price, change ]
+          = values.map(value => numeral(value).value());
+
+        return {
+          date: formattedDate,
+          tradeVolume,
+          tradeValue,
+          transaction,
+          price,
+          change,
+        };
+      })
+      ?.find(data => data.date === date) ?? null;
+  }
+}
